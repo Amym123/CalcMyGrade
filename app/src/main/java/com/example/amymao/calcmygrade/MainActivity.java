ae298b7abd109932a3898a2f36e214b7de9a8770
amymao1 <mao.amy1@gmail.com>
2018-05-19 20:53:41 -0700

    fix bug in Main

--- app/src/main/java/com/example/amymao/calcmygrade/MainActivity.java
+++ app/src/main/java/com/example/amymao/calcmygrade/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.amymao.calcmygrade;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button CurrentGradeCalc;
+    private Button addGradebtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //CurrentGradeCalc calculates what your current grade is given the weights. It directs to another page
+        CurrentGradeCalc = findViewById(R.id.btnCalcCurrentGrade);
+        //when user clicks on button, direct to another page for input weights
+        CurrentGradeCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCurrentGradeCalcActivity();
+
+            }
+        });
+
+        addGradebtn = findViewById(R.id.btnAddGrade);
+        addGradebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    public void openCurrentGradeCalcActivity(){
+        Intent intent = new Intent(this, CurrentGradeCalcActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
